43a8c950e310d200a83f0da961764f4b268eb2a1
Your Name <your-email@example.com>
2026-06-10 13:27:20 +0530

    Added new DSA problems

--- operators.cpp
+++ operators.cpp
@@ -3,5 +3,15 @@ using namespace std;
 
 int main(){
     int n;
-    
+    cin>>n;
+
+    int i = 1;
+    int sum = 0;
+    while(i<=n){
+          sum  = sum + i;
+           i = i + 2;
+           
+            }
+            cout<< " sum of digits : "<<sum<<endl;
+    return 0;
 }
