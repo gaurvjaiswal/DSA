c62a90e7a243202ed398136ad49cbda19b3ea3ce
Gaurav <gsain8470@gmail.com>
2026-07-12 00:00:00 +0530

    Daily DSA practice

--- string.cpp
+++ string.cpp
@@ -1,6 +1,30 @@
 #include<iostream>
 using namespace std;
 
+bool checkPalindrome(char a[], int n){
+    int s = 0;
+    int e = n-1;
+
+    while(s<=e){
+        if(a[s] != a[e] ){
+            return 0;
+        }
+        else{
+            s++;
+            e--;
+        }
+        return 1;
+    }
+}
+
+void reverseString(char name[], int n){
+    int s = 0;
+    int e = n-1 ;
+    while(s<e){
+        swap(name[s++], name[e--]);
+    }
+}
+
 int length(char name[]){
     int count = 0;
     for(int i = 0; name[i] != 0; i++){
@@ -18,6 +42,13 @@ int main(){
     
     cout<<"Your name is " ;
     cout<<name << endl;
-    cout<< "Length of your name is " << length(name) << endl;
+
+    int len = length(name);
+    cout<< "Length of your name is " << len  << endl;
+    reverseString(name , len);
+    cout<<"Your Name is ";
+    cout<< name << endl;
+
+    cout<< "Palindrome or not " << checkPalindrome(name, len);
     return 0;
 }
